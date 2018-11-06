d932de9122b5a9488bd1046f0024c8967895e7d4
Osip Akopiants <31482944+osipnunik@users.noreply.github.com>
2018-11-06 15:26:20 +0200

    Update MainHello.java
    add whitespaces

--- src/main/java/mainpart/MainHello.java
+++ src/main/java/mainpart/MainHello.java
@@ -2,6 +2,6 @@ package mainpart;
 
 public class MainHello {
     public static void main(String[] args) {
-        System.out.println("Main hello");
+        System.out.println("Main hello     ");
     }
 }
