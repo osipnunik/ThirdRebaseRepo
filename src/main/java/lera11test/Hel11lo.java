35b0551b17cdc63b98541e5c9285e4f77d0c0945
Valeriia Chernikova <vchernik@softserveinc.com>
2018-11-07 11:21:06 +0200

    lera`s 25th commit

--- src/main/java/lera11test/Hel11lo.java
+++ src/main/java/lera11test/Hel11lo.java
@@ -2,9 +2,8 @@ package lera11test;
 
 public class Hel11lo {
     public static void main(String[] args) {
-        System.out.println("11111");
-        System.out.println("22222");
+       
         System.out.println("33333");
-      
+        System.out.println("44444");
     }
 }
