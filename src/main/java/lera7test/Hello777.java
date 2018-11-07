6fb6b41a860becc62ba9a9d763680dae419ab02d
Valeriia Chernikova <vchernik@softserveinc.com>
2018-11-07 14:55:31 +0200

    lera`s new com 6

--- src/main/java/lera7test/Hello777.java
+++ src/main/java/lera7test/Hello777.java
@@ -3,8 +3,8 @@ package lera7test;
 public class Hello777 {
     public static void main(String[] args) {
 
-        System.out.println("99999");
-        System.out.println("99999");
+        System.out.println("10");
+        System.out.println("10");
 
     }
 }
