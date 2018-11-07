c6a2adc74146bedbda063d50d120834b527830ec
Valeriia Chernikova <vchernik@softserveinc.com>
2018-11-07 13:45:42 +0200

    3

--- src/main/java/lera11test/Hel11lo.java
+++ src/main/java/lera11test/Hel11lo.java
@@ -2,13 +2,7 @@ package lera11test;
 
 public class Hel11lo {
     public static void main(String[] args) {
-<<<<<<< HEAD
-       
-        System.out.println("55588");
-        System.out.println("55588");
-        System.out.println("66666");
-=======
         System.out.println("Correct Hello1");
->>>>>>> lera`s 15th commit
+        System.out.println("Correct Hello11");
     }
 }
